742824759d8bcf8f6836c3311357dc287995d83f
Natacha Chabas <nat.chabas@hotmail.com>
2020-11-16 02:10:37 +0100

    Suppression de fichiers inutiles + Ajout de commentaires

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,9 +23,10 @@ export class AppComponent implements OnInit {
      * - Appel de la fonction buttonPlay avec la valeur de la touche pressée
      */
     window.addEventListener("load", () => {
-      // a commenter listener clavier
+      // Ecoute de la touche clavier appuyée si elle existe
       window.addEventListener("keypress", (_clavier) => {
 
+        // Association touche clavier au son Mp3
         const _mapClavier = {
           
           'a': 'C2', 'z': 'D2', 'e': 'E2', 'r': 'F2', 't': 'G2', 'y': 'A2', 'u': 'B2', 'i': 'C3', 'o': 'D3',
@@ -35,7 +36,7 @@ export class AppComponent implements OnInit {
           '²': 'Eb2', '&': 'Ds2', 'é': 'Fs2', '"': 'Gb2', '(': 'Bb2', '-': 'Eb3', 'è': 'Ds3', '_': 'Fs3', 'ç': 'Gb3', 
           'à': 'Bb3', ')': 'Eb4', '=': 'Ds4', '/': 'Fs4', '#': 'Gb4', '{': 'Bb4', '[': 'Eb5', '|': 'Ds5', '%': 'Fs5',
           '@': 'Gb5', ']': 'Bb5', '}': 'Eb6', 'µ': 'Ds6', '£': 'Fs6', '¤': 'Gb6', '§': 'Bb6'
-                    
+
         };
 
         this._partition = this.player.buttonPlay(_mapClavier[_clavier["key"]], this._partition);
@@ -44,7 +45,7 @@ export class AppComponent implements OnInit {
   }
 
   /**
-   * Enregistrer
+   * Enregistrer la partition
    */
   enregistrer() {
     const enregistrer = this._partition;
@@ -52,18 +53,19 @@ export class AppComponent implements OnInit {
   }
 
   /**
-   * 
+   * Efface la partition précédente et, enregistre la nouvelle partition
    */
   nouveau() {
     this._partition = [];
   }
 
   /**
-   * 
+   * Lis la partition enregistrée
    */
   lire() {
     this._partition.forEach((note, index) => {
-      // a commenter setTimeout avec * index 
+      // Joue les notes lues avec un intervalle de 500ms pour jouer la note suivante :
+      // -par exemple: la 2 ème note sera jouée à 1000ms [500ms * index (index commençant à 1 correspondant à la première note lue)]
       setTimeout(() => {
         this.player.buttonPlay(note, this._partition);
       }, this._interval * index);
